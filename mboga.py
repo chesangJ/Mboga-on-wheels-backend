5a376544dce178f44a6d5e2068d08d9de7cf3cc2
Muyale Okoyo <annmuyale@gmail.com>
2023-05-16 12:46:49 +0300

    adding items to inventory

--- mboga.py
+++ mboga.py
@@ -71,3 +71,13 @@ class Inventory:
         else:
             print(f"{name} not found in inventory")
 inventory = Inventory()
+inventory.add_to_inventory("banana", 10)
+inventory.add_to_inventory("apples", 5)
+inventory.add_to_inventory("carrots", 20)
+inventory.add_to_inventory("spinach", 40)
+inventory.add_to_inventory("cabbage", 75)
+inventory.add_to_inventory("onions", 70)
+inventory.add_to_inventory("lemons", 90)
+inventory.add_to_inventory("watermelon", 15)
+inventory.add_to_inventory("oranges", 50)
+
