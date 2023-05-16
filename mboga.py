ebddc405a76827dfa8ae01ed7aa0e8e163613bc6
KABANYANA <joyeusekabanyana@gmail.com>
2023-05-16 11:56:31 +0300

    customer sign up and login

--- mboga.py
+++ mboga.py
@@ -1,3 +1,46 @@
+
+class Customer:
+    def __init__(self):
+        email= input('enter your email')
+        print('your email is:',email)
+        # self.email=email
+    def signUp(self):
+        name= input('enter your name')
+        print('your name is:',name)
+
+        
+
+        phone_number= input('enter your phone number')
+        print('your phone number is:',phone_number)
+
+        password= input('enter your password')
+        print('your password is:',password)
+        print(f"your data has been saved")
+    
+
+    def login(self,email):
+        login_email= input('enter your email')
+        if(login_email!= email):
+            print("email not correct")
+        p_number= input('enter your phone number')
+        print('your name is:',p_number)
+
+    
+        location= input('enter your location')
+        print('your location is:',location)
+        
+        print('SUCCESSFULY LOGGED IN')
+
+
+    
+customer1=Customer()
+customer1.signUp()
+print("YOUR ACCOUNT HAS BEEN CREATED, YOU CAN LOGIN")
+customer1.login("joyeuse@gmail.com")
+  
+   
+  
+
 class Stock:
     def __init__(self, name, quantity):
         self.name = name
