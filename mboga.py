4eecaf55ab17e09a39314f0ea5265a85d577a511
Joan Njiru <joanwanjirunjiru@gmail.com>
2023-05-16 13:06:18 +0300

    updating inventory

--- mboga.py
+++ mboga.py
@@ -92,4 +92,17 @@ inventory.add_to_cart("lemons", 4)
 inventory.add_to_cart("garlic", 10)
 inventory.add_to_cart("mangoes", 5)
 
+#Update  the inventory
+inventory.add_to_inventory("cucumbers", 5)
+inventory.add_to_inventory("sukuma wiki", 10)
+inventory.add_to_inventory("onions", 5)
+inventory.add_to_inventory("brocolli", 10)
+inventory.add_to_inventory("apples", 5)
+inventory.add_to_inventory("strawberries", 10)
+inventory.add_to_inventory("carrots", 5)
+inventory.add_to_inventory("pineapples", 10)
+inventory.add_to_inventory("dodo", 5)
+inventory.add_to_inventory("lemons", 10)
+
+
 
