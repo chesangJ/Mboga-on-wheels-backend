a90b7b4359c6c84b3b158d63dd8947e6cb40d2d9
Mutesi-Bridget <bridgettemutesi@gmail.com>
2023-05-16 12:08:15 +0300

    functions to add items to cart and updating inventory

--- mboga.py
+++ mboga.py
@@ -52,3 +52,22 @@ class Stock:
             self.quantity -= quantity
         else:
             print(f"Not enough {self.name} in stock")
+
+
+
+class Inventory:
+    def __init__(self):
+        self.items = {}
+    def add_to_inventory(self, name, quantity):
+        if name in self.items:
+            self.items[name].add_stock(quantity)
+        else:
+            self.items[name] = Stock(name, quantity)
+    def add_to_cart(self, name, quantity):
+        if name in self.items:
+            self.items[name].remove_stock(quantity)
+            print(f"{quantity} {name} added to cart")
+            print(f"Remaining stock for {name}: {self.items[name].quantity}")
+        else:
+            print(f"{name} not found in inventory")
+inventory = Inventory()
