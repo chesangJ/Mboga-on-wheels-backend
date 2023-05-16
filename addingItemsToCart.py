2f2e633ab7d3e709ab4c69863bd3f746f7a9cdb9
Bridget-Mutesi <124131613+Bridget-Mutesi@users.noreply.github.com>
2023-05-16 16:40:49 +0300

    customer adding items from stock to cart

---
+++ addingItemsToCart.py
@@ -0,0 +1,10 @@
+inventory=Inventory()
+# adding items to the cart
+inventory.add_to_cart("banana", 2)
+inventory.add_to_cart("apples", 4)
+inventory.add_to_cart("carrots", 10)
+inventory.add_to_cart("cabbages", 5)
+inventory.add_to_cart("spinach", 2)
+inventory.add_to_cart("lemons", 4)
+inventory.add_to_cart("garlic", 10)
+inventory.add_to_cart("mangoes", 5)
